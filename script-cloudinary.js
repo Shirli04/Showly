99098c2e64f643504e54e20b67c62e79d19b135a
Shirli04 <begmydowsirli@gmail.com>
2026-01-23 16:45:23 +0500

    apk notification v1

--- script-cloudinary.js
+++ script-cloudinary.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', async () => {
+ document.addEventListener('DOMContentLoaded', async () => {
     
     // --- DOM ELEMANLARI ---
     const storeList = document.getElementById('store-list');
